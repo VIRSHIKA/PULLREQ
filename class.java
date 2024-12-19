c3a9edc8404bf3518eb02600e824d26c1bc50c04
virshikarajeshkumar <virshikarajeshkumar@gmail.com>
2024-12-19 10:17:42 +0530

    Update PrimeNumbers.java

--- class.java
+++ class.java
@@ -1,41 +1,22 @@
-class
+class PrimeNumbers 
 {
- String name;
- main()
-system.out.print("constructor call");
-name ="program";
+public static void main(String[] args) 
+{
+int limit = 100;
+System.out.println("Prime numbers between 1 and " + limit);
+for(int i=1; i < 100; i++)
+{
+boolean isPrime = true;
+for(int j=2; j < i ; j++)
+{
+if(i % j == 0)
+{
+isPrime = false;
+break;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+if(isPrime)
+System.out.print(i + " ");
+}
+}
+}
